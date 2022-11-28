343515632cf56a777b38bee4dc4b543443adb37e
Sam-GTECH <sburton@gaming.tech>
2022-11-28 20:02:34 +0100

    Changed index
    index.html is now index.php
    It also uses the required files now

--- index.php
+++ index.php
@@ -19,26 +19,11 @@
 </head>
 
 <body>
+    <?php require_once "php/requires/config.php" ?>
 
     <img id="riped" class="hide-on-med-and-up" src="img/gamingcampusriped.png" alt="Gaming Campus">
 
-    <nav>
-        <div class="nav-wrapper grey darken-4">
-            <a class="brand-logo left hide-on-med-and-down">Portfolio</a>
-            <a class="brand-logo center hide-on-large-only">Portfolio</a>
-            <ul id="nav-mobile" class="right hide-on-med-and-down">
-                <li><a href="projects/why-play-undertale.html">Projet HTML+CSS</a></li>
-                <li><a href="projects/frozen-heart.html">Frozen Heart</a></li>
-                <li><a href="projects/shmeup.html">Shmeup</a></li>
-            </ul>
-        </div>
-    </nav>
-
-    <ul class="sidenav" id="mobile-demo">
-        <li><a href="projects/why-play-undertale.html">Projet HTML+CSS</a></li>
-        <li><a href="projects/frozen-heart.html">Frozen Heart</a></li>
-        <li><a href="projects/shmeup.html">Shmeup</a></li>
-    </ul>
+    <?php require_once "php/requires/nav.php" ?>
 
     <div class="start">
 
@@ -209,50 +194,7 @@
         </div>
     </div>
 
-    <footer class="page-footer black">
-        <div class="container">
-            <div class="row">
-                <div class="col l4 s12 orange-text">
-                    <p class="purple-text bigger">Professional Footer</p>
-                    <p class="footer-easter">This is a professional footer with serious text.</p>
-                </div>
-                <div class="col l2 offset-l2 s12">
-                    <p class="purple-text bigger">Simbel</p>
-                    <ul>
-                        <li><a class="white-text" href="https://www.youtube.com/watch?v=7TD0rTKcmWQ"><i
-                                    class="fa-brands fa-twitter"></i></a></li>
-                        <li><a class="white-text" href="https://www.youtube.com/watch?v=7TD0rTKcmWQ"><i
-                                    class="fa-brands fa-instagram"></i></a></li>
-                        <li><a class="white-text" href="https://www.youtube.com/watch?v=7TD0rTKcmWQ"><i
-                                    class="fa-brands fa-facebook"></i></a></li>
-                        <li><a class="white-text" href="https://www.youtube.com/watch?v=7TD0rTKcmWQ"><i
-                                    class="fa-brands fa-linkedin"></i></a></li>
-                    </ul>
-                </div>
-                <div class="col l2 offset-l2 s12">
-                    <p class="purple-text bigger">Deadly</p>
-                    <ul>
-                        <li><a class="white-text" href="https://www.youtube.com/watch?v=7TD0rTKcmWQ"><i
-                                    class="fa-brands fa-twitter"></i></a></li>
-                        <li><a class="white-text" href="https://www.youtube.com/watch?v=7TD0rTKcmWQ"><i
-                                    class="fa-brands fa-instagram"></i></a></li>
-                        <li><a class="white-text" href="https://www.youtube.com/watch?v=7TD0rTKcmWQ"><i
-                                    class="fa-brands fa-facebook"></i></a></li>
-                        <li><a class="white-text" href="https://www.youtube.com/watch?v=7TD0rTKcmWQ"><i
-                                    class="fa-brands fa-linkedin"></i></a></li>
-                    </ul>
-                </div>
-            </div>
-        </div>
-        <div class="footer-copyright">
-            <div class="container">
-                <span class="purple-text">© 2022 Gaming Campus</span>
-                <a class='white-text'
-                    href='https://gamingcampus.fr/?utm_term=gaming%20campus&utm_campaign=Gaming+Campus+-+Marque&utm_source=adwords&utm_medium=ppc&hsa_acc=5656227748&hsa_cam=11532002367&hsa_grp=118137353731&hsa_ad=483696679366&hsa_src=g&hsa_tgt=kwd-423735683440&hsa_kw=gaming%20campus&hsa_mt=e&hsa_net=adwords&hsa_ver=3&gclid=Cj0KCQjw48OaBhDWARIsAMd966DFWlqTIXOURakWuZW0UHO8Yav7JR7Avvwrok1ZCtzEWXvwaswAcocaAlp3EALw_wcB'><i
-                        class="fa-sharp fa-solid fa-link"></i></a>
-            </div>
-        </div>
-    </footer>
+    <?php require_once "php/requires/footer.php" ?>
 
     <!--JavaScript at end of body for optimized loading-->
     <script src="js/jquery.min.js"></script>
